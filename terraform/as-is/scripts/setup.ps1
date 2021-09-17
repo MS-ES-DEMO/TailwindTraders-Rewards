17ae2f91ac8f1a5209ad83895c5e47c46c850933
cmendible <cmendible@microsoft.com>
2021-09-17 12:55:17 +0200

    added Azure Migrate App Containerization

--- terraform/as-is/scripts/setup.ps1
+++ terraform/as-is/scripts/setup.ps1
@@ -1,8 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 
 # Get Edge
-$get_edge_location="https://raw.githubusercontent.com/matbe/PowerShell/master/Other/Get-EdgeEnterpriseMSI.ps1"
-Invoke-WebRequest -Uri $get_edge_location -OutFile get-edge.ps1 -UseBasicParsing
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/matbe/PowerShell/master/Other/Get-EdgeEnterpriseMSI.ps1" -OutFile get-edge.ps1 -UseBasicParsing
 .\get-edge.ps1 Stable .
 
 # Install Edge
@@ -24,11 +23,29 @@ Install-WindowsFeature Web-WebSockets
 Install-WindowsFeature Web-Mgmt-Tools
 Install-WindowsFeature Web-Scripting-Tools
 Install-WindowsFeature Web-Mgmt-Service
+Install-WindowsFeature WinRM-IIS-Ext
+
+# Get webdeploy
+Invoke-WebRequest -Uri "https://aka.ms/webdeploy3.6" -OutFile webdeploy.msi -UseBasicParsing
+
+# Install webdeploy
+Start-Process powershell.exe -ArgumentList '.\webdeploy.msi', '/q' -Wait
+
+# Cleanup webdeploy msi
+rm .\webdeploy.msi
+
+# Get AppContainerizationInstaller
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=2134571" -OutFile AppContainerizationInstaller.ps1 -UseBasicParsing
+
+# Install AppContainerizationInstaller
+.\AppContainerizationInstaller.ps1
+
+# Cleanup AppContainerizationInstaller
+rm .\AppContainerizationInstaller.msi
 
 $run_on_logon = {
 # Get SQL Express
-$sql_express_location="https://go.microsoft.com/fwlink/?linkid=866658"
-Invoke-WebRequest -Uri $sql_express_location -OutFile sql_express_setup.exe -UseBasicParsing 
+Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=866658" -OutFile sql_express_setup.exe -UseBasicParsing 
 
 # Install SQL Express
 . .\sql_express_setup.exe /Q /IACCEPTSQLSERVERLICENSETERMS /ACTION="Install" /ENU /INSTALLPATH="C:\Program Files\Microsoft SQL Server" | Write-Output
@@ -41,8 +58,7 @@ sqlcmd.exe -E -S .\SQLEXPRESS -Q "CREATE DATABASE rewardsdb"
 sqlcmd.exe -E -S .\SQLEXPRESS -Q "USE rewardsdb; GRANT execute on schema :: dbo to [IIS APPPOOL\DefaultAppPool]"
 sqlcmd.exe -E -S .\SQLEXPRESS -Q "USE rewardsdb; ALTER ROLE db_owner ADD MEMBER [IIS APPPOOL\DefaultAppPool]"
 
-$sql_script="https://raw.githubusercontent.com/MS-ES-DEMO/TailwindTraders-Rewards/main/Source/SQLScripts/CreateTablesAndPopulate.sql"
-Invoke-WebRequest -Uri $sql_script -OutFile sql_script.sql -UseBasicParsing
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MS-ES-DEMO/TailwindTraders-Rewards/main/Source/SQLScripts/CreateTablesAndPopulate.sql" -OutFile sql_script.sql -UseBasicParsing
 sqlcmd.exe -E -S .\SQLEXPRESS -i .\sql_script.sql
 
 # Cleanup Script
@@ -52,8 +68,7 @@ rm .\sql_script.sql
 rm C:\inetpub\wwwroot\*.*
 rm -r -fo C:\inetpub\wwwroot\aspnet_client\
 
-$app="https://github.com/MS-ES-DEMO/TailwindTraders-Rewards/releases/download/v0.1.0/TailwindTradersBundle.zip"
-Invoke-WebRequest -Uri $app -OutFile TailwindTradersBundle.zip -UseBasicParsing
+Invoke-WebRequest -Uri "https://github.com/MS-ES-DEMO/TailwindTraders-Rewards/releases/download/v0.1.0/TailwindTradersBundle.zip" -OutFile TailwindTradersBundle.zip -UseBasicParsing
 Expand-Archive -Path .\TailwindTradersBundle.zip -DestinationPath C:\inetpub\wwwroot\
 
 # Cleanup Zip
