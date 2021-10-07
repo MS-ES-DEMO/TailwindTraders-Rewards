58c5b7f66331506e2a7c93a41ca9313a2f4d27ef
cmendible <cmendible@microsoft.com>
2021-10-07 19:39:35 +0200

    scaled vm size

--- terraform/as-is/main.tf
+++ terraform/as-is/main.tf
@@ -78,7 +78,7 @@ resource "azurerm_virtual_machine" "vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_D2s_v3"
+  vm_size               = "Standard_D8s_v3"
 
   storage_image_reference {
     publisher = "MicrosoftWindowsServer"
